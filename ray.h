6516ef0c409982d43a95823616478f39df942faa
larix <greiner.roli@gmail.com>
2026-09-08 22:38:56 +0200

    book one finished

--- ray.h
+++ ray.h
@@ -17,7 +17,7 @@ public:
     const vec3& direction() const { return dir; }
 
     point3 at(double t) const {
-        return  orig + t*dir;
+        return  orig + t * dir;
     }
 
 private:
--- vec3.h
+++ vec3.h
@@ -35,7 +35,7 @@ public:
     }
 
     vec3& operator/=(double t) {
-        return *this *= 1/t;
+        return *this *= 1 / t;
     }
 
     double length() const {
@@ -43,7 +43,20 @@ public:
     }
 
     double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+    bool near_zero() const {
+        auto s = 1e-8;
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+    }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 };
 
@@ -66,7 +79,7 @@ inline vec3 operator*(const vec3& u, const vec3& v) {
 }
 
 inline vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
 }
 
 inline vec3 operator*(const vec3& v, double t) {
@@ -74,7 +87,7 @@ inline vec3 operator*(const vec3& v, double t) {
 }
 
 inline vec3 operator/(const vec3& v, double t) {
-    return (1/t) * v;
+    return (1 / t) * v;
 }
 
 inline double dot(const vec3& u, const vec3& v) {
@@ -91,6 +104,42 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        auto lensq = p.length_squared();
+        if (1e-160 < lensq && lensq <= 1)
+            return p / sqrt(lensq);
+    }
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0)
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 #endif //VEC3_H
