cd6cd37885b66c4c68bc196e79f53456d06d7e8f
larix <greiner.roli@gmail.com>
2026-09-09 19:08:46 +0200

    textures and aabb with bvh

--- hittable.h
+++ hittable.h
@@ -5,6 +5,8 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "aabb.h"
+
 class material;
 
 class hit_record {
@@ -13,6 +15,8 @@ public:
     vec3 normal;
     shared_ptr<material> mat;
     double t;
+    double u;
+    double v;
     bool front_face;
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
@@ -26,6 +30,8 @@ public:
     virtual ~hittable() = default;
 
     virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+    virtual aabb bounding_box() const = 0;
 };
 
 #endif //HITTABLE_H
